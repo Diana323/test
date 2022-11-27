325067de030adb8d26b8ef06eb737af454b3948d
Diana323 <110489496+Diana323@users.noreply.github.com>
2022-11-27 19:44:06 +0200

    .....

--- script.js
+++ script.js
@@ -42,17 +42,88 @@
 //   return true;
 // }
 
-               //   сума властивостей обьєкта
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("body"),
+  };
 
-let salaries = {
-    john: 100,
-    ann: 160,
-    pete: 130,
-}
-let sum = 0;
-for (let key in salaries) {
-    sum += salaries[key];
-}
-// alert(sum);
-console.log(sum)
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
+////////////////////////// резервная / модалка//////////////////////////////////////////////
+// var modal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var button = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// // When the user clicks on <btn> (x), close the modal
+// button.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+                   //////// моя модалка........///////
+// document.addEventListener('DOMContentLoaded', function() {
+//   var modalButtons = document.querySelectorAll('.js-open-modal'),
+//       overlay      = document.querySelector('#overlay-modal'),
+//       closeButtons = document.querySelector('.js-modal-close');
+  
+  
+//   modalButtons.forEach(function(item){
+     
+//      item.addEventListener('click', function(e) {
+        
+//         e.preventDefault();
+//         var modalId = this.getAttribute('data-modal'),
+//             modalElem = document.querySelector('.modal[data-modal="' + modalId +'"]');
+        
+//         modalElem.classList.add('active');
+//         overlay.classList.add('active');
+//         closeButtons.forEach(function(item) {
+//           item.addEventListener('click', function(e) {
+//           });
+      
+//         }); // end foreach
+//      }); // end click
+//   }); // end foreach
+// }); // end ready
+const openBtn = document.querySelector('#open'),
+modal = document.querySelector('#modal'),
+closeBtn = document.querySelector('#close');
+
+openBtn.addEventListener('click', () => {
+modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+modal.style.display = 'none';
+});
+
+window.addEventListener("click", (e) => {
+const target = e.target;
+if (target === modal) {
+modal.style.display = "none";
+}
+});
